c1527f0ba90a54e630bef7f8ebc4f66302a62e0c
sensiz <dev.federicosensi@gmail.com>
2022-11-09 16:30:50 +0100

    final commit + BONUS

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+    <div class="jumbo">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, praesentium? Molestiae placeat sequi
+            maiores doloribus minus voluptates. Exercitationem vel unde nulla. Ea et pariatur exercitationem sint iusto
+            labore aut natus?
+        </p>
+        <img src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style>
+p {
+    padding: 5rem 5rem 0 5rem;
+}
+
+.jumbo {
+    background-color: coral;
+}
+
+img {
+    margin-bottom: 3rem;
+    height: 100px;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+    <div class="title">
+        <h1>My first app with Vite!</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
